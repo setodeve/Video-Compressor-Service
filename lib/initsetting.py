7e11000761110b32f5b0fa140fa1b1400f8eab36
setodeve <keinick11@outlook.com>
2024-01-06 08:22:21 +0900

    mp4処理に変更

--- lib/initsetting.py
+++ lib/initsetting.py
@@ -5,9 +5,15 @@ sys.dont_write_bytecode = True
 
 
 class InetSetting:
+    POSITIVE = 0
+    NEGATIVE = 1
+
     def __init__(self, address, port, type) -> None:
         self.address = address
         self.port = port
         self.setinfo = (address, port)
         self.sock = socket.socket(socket.AF_INET, type)
         self.size = 1400
+        self.text_size = 1024
+        self.sock.settimeout(20)
+        self.file_size = 0
